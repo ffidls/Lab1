be2a82b74db05e9267fa67c4ca63d06582cd27db
ffidls <85582845+ffidls@users.noreply.github.com>
2026-02-08 16:37:07 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author Alina Orekhova, aorekhova@student.sdccd.edu 
+ * @since 02.08.2026
+ * @version v1.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("Alina Orekhova");
+    }
+}
